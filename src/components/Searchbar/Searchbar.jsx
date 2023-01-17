5fd15a57ad418c187e55227b766a0437fd79e34c
Denys Zabolotskyi <d.zabolotskyi@gmail.com>
2023-01-17 20:38:48 +0200

    searchbar completed hooks

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { toast } from 'react-hot-toast';
 import PropTypes from 'prop-types';
 
@@ -11,49 +11,49 @@ import {
   SearchFormButtonLabel,
 } from './Searchbar.styled';
 
-export class Searchbar extends Component {
-  state = {
-    searchName: '',
+export const Searchbar = ({ onSubmit }) => {
+  const [searchName, setSearchName] = useState('');
+  // state = {
+  //   searchName: '',
+  // };
+
+  const handleNameChange = evt => {
+    const target = evt.currentTarget.value.toLowerCase();
+    setSearchName(target);
   };
 
-  handleNameChange = evt => {
-    this.setState({ searchName: evt.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmit = evt => {
+  const handleSubmit = evt => {
     evt.preventDefault();
-    if (this.state.searchName.trim() === '') {
+    if (searchName.trim() === '') {
       toast.error('Please, enter search word!', {
         position: 'top-center',
         duration: 2000,
       });
       return;
     }
-    this.props.onSubmit(this.state.searchName);
-    this.setState({ searchName: '' });
+    onSubmit(searchName);
+    setSearchName('');
   };
 
-  render() {
-    return (
-      <SearchbarBox>
-        <SearchForm onSubmit={this.handleSubmit}>
-          <SearchFormButton type="submit">
-            <SearchFormButtonLabel>Search</SearchFormButtonLabel>
-            <AiOutlineSearch size={24} />
-          </SearchFormButton>
-          <SearchFormInput
-            type="text"
-            autocomplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={this.state.searchName}
-            onChange={this.handleNameChange}
-          />
-        </SearchForm>
-      </SearchbarBox>
-    );
-  }
-}
+  return (
+    <SearchbarBox>
+      <SearchForm onSubmit={handleSubmit}>
+        <SearchFormButton type="submit">
+          <SearchFormButtonLabel>Search</SearchFormButtonLabel>
+          <AiOutlineSearch size={24} />
+        </SearchFormButton>
+        <SearchFormInput
+          type="text"
+          autocomplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={searchName}
+          onChange={handleNameChange}
+        />
+      </SearchForm>
+    </SearchbarBox>
+  );
+};
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
